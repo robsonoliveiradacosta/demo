e1aa19278706b0a16e0c2c8433fe0406545be4b1
Robson Oliveira Da Costa <robsondeveloper@gmail.com>
2021-09-04 14:51:34 -0400

    ci cd

--- src/main/java/com/example/demo/DateTimeResource.java
+++ src/main/java/com/example/demo/DateTimeResource.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ public class DateTimeResource {
 
 	@GetMapping(path = "/now")
 	public String now() {
-		return LocalDateTime.now().toString();
+		return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
 	}
 
 }
